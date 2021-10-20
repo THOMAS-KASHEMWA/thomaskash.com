10ffc6b021a9e5b04bbca4283b5ee629d879afb3
KieranDelobbe <92739767+KieranDelobbe@users.noreply.github.com>
2021-10-20 17:55:36 +0200

    Update placeDeCine.js

--- placeDeCine.js
+++ placeDeCine.js
@@ -1,1 +1,16 @@
 "use strict"
+
+function projet () {
+//variable nbre de place par salle
+    let place1 = ["libre"];
+    let place2 = ["occupée"];
+    let place3 = ["occupée"];
+    let place4 = ["libre"];
+//variable salle
+    let salle1 = [place1, place2, place3, place4];
+    let salle2 = [];
+
+
+//variable nbre de place du client
+    let nbrPlace = +prompt("combien de place désirez-vous?")
+}
