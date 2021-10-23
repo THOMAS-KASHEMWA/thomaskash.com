dcd71d4c087dffbfde2681667ffccc20793286c0
KieranDelobbe <92739767+KieranDelobbe@users.noreply.github.com>
2021-10-23 17:30:21 +0200

    Update placeDeCine.js

--- placeDeCine.js
+++ placeDeCine.js
@@ -25,31 +25,26 @@ return salle1
 
 -------------------------------------------------
 
-let salle1 = ["dune"];
-
-
 function dune(){
-
-    let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-    for (let i = 0; i <= 20; i++) {
+    let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let salle1 = ["dune"];
+    for (let i = 0; i < 20; i++) {
         salle1.push( "n°" + tab[i]);
+
+        for (let i = 0; i < salle1.length; i++) {
+            if (Math.random() < 0.7){
+                salle1[i] = ["libre"];
+            }
+            else {
+                salle1[i] = ["occupée"];
+            }
+
+            salle1[i][1]="n°" + i;
+        }
     }
     console.log(salle1);
-    for (let i = 0; i < salle1.length; i++) {
-       if (Math.random() < 0.5){
-           salle1[i] = ["libre"];
-       }
-       else {
-           salle1[i] = ["occupée"];
-       }
-
-        salle1[i][1]="n°" + i;
-    }
-
 }
 
-console.log(salle1);
 function projet () {
 
 //variable nbre de place du client et choix du film
