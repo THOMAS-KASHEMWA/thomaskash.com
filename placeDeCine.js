9b9d0c9ced241502c377f3482b4480319b4fb119
KieranDelobbe <92739767+KieranDelobbe@users.noreply.github.com>
2021-10-23 17:24:42 +0200

    Update placeDeCine.js

--- placeDeCine.js
+++ placeDeCine.js
@@ -1,68 +1,6 @@
-// "use strict"
 
-// function projet () {
+/* presque fini
 
-//     /**  
-//  * ESSAI 1
-//  * 
-//  * code pour créer les places
-// let tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-// let salle1 = ""
-// for (let i = 0; i <= 20; i++) {
-//     salle1 = salle1 + "['A" + tab[i] + "'], "
-// }
-
-
-// for (let i = 0; i < salle1.length; i++) {
-//     salle1[i][0] = "libre"
-// }
-
-
-
-
-
-// //ESSAI 2
-
-// let tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-// let salle1 = []
-// for (let i = 0; i <= 20; i++) {
-//     salle1.push( 'A' + tab[i])
-// }
-
-// for (let i = 0; i < salle1.length; i++) {
-//     salle1[i] = ["libre"]
-// }
-
-// /** 
-//  * ESSAI 3
-// let tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-// let salle1 = ""
-// for (let i = 0; i <= 20; i++) {
-//     if (i != 20) {
-//         salle1 = salle1 + "['A" + tab[i] + "'], "
-//     }
-//     else {
-//         salle1 = salle1 + "['A" + tab[i] + "']"
-//     }
-// }
-
-// **/
-
-    
-    
-//     //variable nbre de place par salle
-//     let place1 = ["libre"];
-//     let place2 = ["occupée"];
-//     let place3 = ["occupée"];
-//     let place4 = ["libre"];
-// //variable salle
-//     let salle1 = [place1, place2, place3, place4];
-//     let salle2 = [];
-
-
-// //variable nbre de place du client
-//     let nbrPlace = +prompt("combien de place désirez-vous?")
-// }
 "use strict"
 
 let salle1 = ["dune"];
@@ -110,3 +48,5 @@ function projet () {
     }
     console.log(salle1);
 }
+
+*/
