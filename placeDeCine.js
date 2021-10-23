6879a052d3eb969b9b1029db62bbbc0fda15da0a
THOMAS-KASHEMWA <92874074+THOMAS-KASHEMWA@users.noreply.github.com>
2021-10-23 17:26:34 +0200

    Update placeDeCine.js

--- placeDeCine.js
+++ placeDeCine.js
@@ -3,7 +3,30 @@
 
 "use strict"
 
+---------------------------------------------------
+function creerSalle1 () {
+    let tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let salle1 = ["dune"]
+for (let i = 0; i < 20; i++) {
+    salle1.push( 'A' + tab[i])
+}
+
+for (let i = 1; i < salle1.length; i++) {
+    if (Math.random() < 0.5){
+        salle1[i] = ["libre"];
+    }
+    else {
+        salle1[i] = ["occupée"];
+    }
+    salle1[i][1]="n°" + i;
+}
+return salle1
+}
+
+-------------------------------------------------
+
 let salle1 = ["dune"];
+
 
 function dune(){
 
